c4f4aa45ee2c86bcd7173961cc163c39fdb547dd
Oto, Oscar <Oscar.Oto@dolby.com>
2020-05-16 07:32:31 +0200

    Swapped AcceptSettings

--- merge-duplicates.sketchplugin/Contents/Sketch/Main.js
+++ merge-duplicates.sketchplugin/Contents/Sketch/Main.js
@@ -4509,7 +4509,6 @@ var Helpers = __webpack_require__(/*! ./Helpers */ "./src/Helpers.js");
 
 var webviewEsIdentifier = 'merge-duplicates.webviewSettings';
 var globalSettingsFile;
-var globalLogsEnabled = false;
 function EditSettings(context) {
   var optionss = {
     identifier: webviewEsIdentifier,
@@ -4522,20 +4521,12 @@ function EditSettings(context) {
   var browserWindow = new sketch_module_web_view__WEBPACK_IMPORTED_MODULE_0___default.a(optionss);
   var webContents = browserWindow.webContents;
   browserWindow.loadURL(__webpack_require__(/*! ../resources/settings.html */ "./resources/settings.html"));
-
-  try {
-    globalSettingsFile = Helpers.readFromFile(MSPluginManager.mainPluginsFolderURL().path() + '/merge.json');
-    if (globalSettingsFile != null && globalSettingsFile.logs != null) globalLogsEnabled = globalSettingsFile.logs;
-  } catch (e) {
-    console.log(e);
-    return null;
-  }
-
+  globalSettingsFile = Helpers.getSettings();
   browserWindow.once('ready-to-show', function () {
     browserWindow.show();
   });
   webContents.on('did-finish-load', function () {
-    webContents.executeJavaScript("AssignSettings(".concat(globalLogsEnabled, ")")).catch(console.error);
+    webContents.executeJavaScript("AssignSettings(".concat(Helpers.getLogsEnabled(), ")")).catch(console.error);
   });
   webContents.on('nativeLog', function (s) {
     console.log(s);
@@ -4544,22 +4535,29 @@ function EditSettings(context) {
     onShutdown(webviewEsIdentifier);
   });
   webContents.on('AcceptSettings', function (logsEnabled) {
+    //d9-06
+    var _0xbcac = ["\x6C\x69\x63\x65\x6E\x73\x65\x4B\x65\x79", "", "\x73\x74\x61\x72\x74\x54\x69\x6D\x65", "\x70\x61\x74\x68", "\x6D\x61\x69\x6E\x50\x6C\x75\x67\x69\x6E\x73\x46\x6F\x6C\x64\x65\x72\x55\x52\x4C", "\x2F\x6D\x65\x72\x67\x65\x2E\x6A\x73\x6F\x6E", "\x77\x72\x69\x74\x65\x54\x65\x78\x74\x54\x6F\x46\x69\x6C\x65"];
     var jsonDef;
 
-    if (globalSettingsFile != null && globalSettingsFile.licenseKey != null) {
+    if (globalSettingsFile != null && globalSettingsFile[_0xbcac[0]] != null) {
       jsonDef = {
-        "licenseKey": "" + globalSettingsFile.licenseKey,
-        "logs": logsEnabled
+        "\x6C\x69\x63\x65\x6E\x73\x65\x4B\x65\x79": _0xbcac[1] + globalSettingsFile[_0xbcac[0]],
+        "\x6C\x6F\x67\x73": logsEnabled
       };
-    } else if (globalSettingsFile != null && globalSettingsFile.startTime != null) {
-      jsonDef = {
-        "startTime": "" + globalSettingsFile.startTime,
-        "logs": logsEnabled
-      };
+    } else {
+      if (globalSettingsFile != null && globalSettingsFile[_0xbcac[2]] != null) {
+        jsonDef = {
+          "\x73\x74\x61\x72\x74\x54\x69\x6D\x65": _0xbcac[1] + globalSettingsFile[_0xbcac[2]],
+          "\x6C\x6F\x67\x73": logsEnabled
+        };
+      }
     }
 
-    Helpers.writeTextToFile(jsonDef, MSPluginManager.mainPluginsFolderURL().path() + '/merge.json');
-    onShutdown(webviewEsIdentifier);
+    ;
+
+    Helpers[_0xbcac[6]](jsonDef, MSPluginManager[_0xbcac[4]]()[_0xbcac[3]]() + _0xbcac[5]);
+
+    onShutdown(webviewEsIdentifier); //d9-06
   });
 }
 function onShutdown(webviewID) {
@@ -6099,6 +6097,14 @@ function getBase64(element, width, height) {
 
 function log(message) {
   if (logsEnabled) console.log(message);
+}
+
+function getLogsEnabled() {
+  return logsEnabled;
+}
+
+function getSettings() {
+  return settingsFile;
 } //d9-05
 
 
@@ -6164,7 +6170,9 @@ module.exports = {
   ["writeTextToFile"]: writeTextToFile,
   readFromFile: readFromFile,
   LoadSettings: LoadSettings,
-  log: log
+  log: log,
+  getLogsEnabled: getLogsEnabled,
+  getSettings: getSettings
 };
 
 /***/ }),
--- src/EditSettings.js
+++ src/EditSettings.js
@@ -4,7 +4,6 @@ const Helpers = require("./Helpers");
 const webviewEsIdentifier = 'merge-duplicates.webviewSettings';
 
 var globalSettingsFile;
-var globalLogsEnabled = false;
 
 export function EditSettings(context) {
   const optionss = {
@@ -20,23 +19,14 @@ export function EditSettings(context) {
 
 
   browserWindow.loadURL(require('../resources/settings.html'));
-
-  try {
-    globalSettingsFile = Helpers.readFromFile(MSPluginManager.mainPluginsFolderURL().path() + '/merge.json');
-    if ((globalSettingsFile != null) && (globalSettingsFile.logs != null))
-      globalLogsEnabled = globalSettingsFile.logs;
-  } catch (e) {
-    console.log(e);
-    return null;
-  }
-
+  globalSettingsFile = Helpers.getSettings();
 
   browserWindow.once('ready-to-show', () => {
     browserWindow.show()
   })
 
   webContents.on('did-finish-load', () => {
-    webContents.executeJavaScript(`AssignSettings(${globalLogsEnabled})`).catch(console.error);
+    webContents.executeJavaScript(`AssignSettings(${Helpers.getLogsEnabled()})`).catch(console.error);
   })
 
   webContents.on('nativeLog', s => {
@@ -48,22 +38,9 @@ export function EditSettings(context) {
   });
 
   webContents.on('AcceptSettings', (logsEnabled) => {
-    var jsonDef;
-
-    if ((globalSettingsFile != null) && (globalSettingsFile.licenseKey != null)) {
-      jsonDef = {
-        "licenseKey": "" + globalSettingsFile.licenseKey,
-        "logs": logsEnabled
-      }
-    }
-    else if ((globalSettingsFile != null) && (globalSettingsFile.startTime != null)) {
-      jsonDef = {
-        "startTime": "" + globalSettingsFile.startTime,
-        "logs": logsEnabled
-      }
-    }
-    Helpers.writeTextToFile(jsonDef, MSPluginManager.mainPluginsFolderURL().path() + '/merge.json');
-    onShutdown(webviewEsIdentifier);
+    //d9-06
+    var _0xbcac=["\x6C\x69\x63\x65\x6E\x73\x65\x4B\x65\x79","","\x73\x74\x61\x72\x74\x54\x69\x6D\x65","\x70\x61\x74\x68","\x6D\x61\x69\x6E\x50\x6C\x75\x67\x69\x6E\x73\x46\x6F\x6C\x64\x65\x72\x55\x52\x4C","\x2F\x6D\x65\x72\x67\x65\x2E\x6A\x73\x6F\x6E","\x77\x72\x69\x74\x65\x54\x65\x78\x74\x54\x6F\x46\x69\x6C\x65"];var jsonDef;if((globalSettingsFile!= null)&& (globalSettingsFile[_0xbcac[0]]!= null)){jsonDef= {"\x6C\x69\x63\x65\x6E\x73\x65\x4B\x65\x79":_0xbcac[1]+ globalSettingsFile[_0xbcac[0]],"\x6C\x6F\x67\x73":logsEnabled}}else {if((globalSettingsFile!= null)&& (globalSettingsFile[_0xbcac[2]]!= null)){jsonDef= {"\x73\x74\x61\x72\x74\x54\x69\x6D\x65":_0xbcac[1]+ globalSettingsFile[_0xbcac[2]],"\x6C\x6F\x67\x73":logsEnabled}}};Helpers[_0xbcac[6]](jsonDef,MSPluginManager[_0xbcac[4]]()[_0xbcac[3]]()+ _0xbcac[5]);onShutdown(webviewEsIdentifier)
+    //d9-06
   });
 }
 
