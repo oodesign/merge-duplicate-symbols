ff743c5dae12af62f729040a8ec2138428071614
oodesign <oscar@oodesign.me>
2021-01-19 10:14:32 +0100

    Added license support link

--- src/Help.js
+++ src/Help.js
@@ -1,10 +1,26 @@
-function documentation() {
+export function documentation() {
   NSWorkspace.sharedWorkspace().openURL(NSURL.URLWithString("http://www.mergeduplicates.com/"));
 }
 
-function report_issue() {
+export function report_issue() {
   NSWorkspace.sharedWorkspace().openURL(NSURL.URLWithString("https://github.com/oodesign/merge-duplicate-symbols/issues"));
 }
 
-module.exports = { documentation,report_issue };
-
+export function report_licensing() {
+  
+  var subject=encodeURI("License issues");
+  var body=encodeURI("\
+Hi!\
+\n\n\
+Looks like something is not working properly with my Merge Duplicates license.\
+\n\n\
+This is the e-mail account I used to get my license on Gumroad:\
+\n\
+And this is my license key:\
+\n\n\
+May you please take a look and come back to me as soon as possible?\
+\n\n\
+Thank you!\
+  ");
+  NSWorkspace.sharedWorkspace().openURL(NSURL.URLWithString("mailto:licensing@oodesign.me?subject="+subject+"&body="+body));
+}
